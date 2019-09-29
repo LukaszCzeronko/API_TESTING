e0c4f989e9f742f4f9942de4526114df10fb9c8b
LukaszCzeronko <czeronko.l@gmail.com>
2019-09-29 17:23:14 +0200

    Fixing review comments

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # API_TESTING
 BASIC JAVA PROJECT FOR TESTING REST API
 
+The main task of our project is to test working functionality of the weather web service(https://weather.api.here.com).
+
 # Introduction
-This project is for
+In our project we want to do validation and functional tests to confirm that the web service (https://weather.api.here.com) is working properly. Our test strategy will be based on methodical and analytical approach to tests to check if the API is responding properly according to the documentation. Most of the test will be focused on testing path and query parameters.
 
 # Installation
 run 
--- src/test/java/validation/HttpMethodsTest.java
+++ src/test/java/validation/HttpMethodsTest.java
@@ -1,47 +1,38 @@
-package main;
+package validation;
 
-import io.restassured.RestAssured;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 
 public class HttpMethodsTest {
 
-  // @BeforeClass
-  //  public final void setBaseUriPath() {
-  //    RestAssured.baseURI = "https://weather.api.here.com";
-  //    RestAssured.basePath = "/weather/1.0/report.json";
-  //  }
-
   private static String webPath =
       "https://weather.api.here.com/weather/1.0/report.json?app_id=JIlgIjxb334PrWXpDC3w&app_code=QZvw9AhazmUb1tY3uX40DQ&product=observation&name=Berlin";
 
-
-
   @DataProvider(name = "dataSetCodeStatus")
   public static Object[][] setCodeStatus() {
-    String[][] StatusCode = new String[][] {{"GET", "200"}, {"POST", "405"},{"DELETE","405"},{"OPTIONS","405"},{"PUT","405"},{"HEAD","200"},{"TRACE","403"},{"PATCH","405"}};
+    String[][] StatusCode =
+        new String[][] {
+          {"GET", "200"},
+          {"POST", "405"},
+          {"DELETE", "405"},
+          {"OPTIONS", "405"},
+          {"PUT", "405"},
+          {"HEAD", "200"},
+          {"TRACE", "403"},
+          {"PATCH", "405"}
+        };
     return StatusCode;
   }
 
   @Test(dataProvider = "dataSetCodeStatus")
-  public static void testHTTPMethods(
-      String methodName,
-      String allowedMethod) {
+  public static void testHTTPMethods(String methodName, String httpCode) {
 
     Response response = given().when().request(methodName, webPath);
-   
 
-    assertEquals(String.valueOf(response.statusCode()), allowedMethod);
-
+    assertEquals(String.valueOf(response.statusCode()), httpCode,"wrong response status code");
   }
-
-
 }
