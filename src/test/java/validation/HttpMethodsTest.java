a6b807a94308912d5f434645949b78efd198b683
LukaszCzeronko <czeronko.l@gmail.com>
2019-10-04 18:46:38 +0200

    HttpMethodsTestFix

--- src/test/java/validation/HttpMethodsTest.java
+++ src/test/java/validation/HttpMethodsTest.java
@@ -1,37 +1,31 @@
 package validation;
-
 import io.restassured.response.Response;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
-
 public class HttpMethodsTest {
-
   private static final String BASE_URL =
       "https://weather.api.here.com/weather/1.0/report.json?app_id=JIlgIjxb334PrWXpDC3w&app_code=QZvw9AhazmUb1tY3uX40DQ&product=observation&name=Berlin";
-
   @DataProvider(name = "dataSetStatusCodes")
   public Object[][] setCodeStatus() {
     Object[][] statusCode =
         new Object[][] {
-          {"TC_2.1","GET", 200},//DOROBIĆ
-          {"POST", 405},
-          {"DELETE", 405},
-          {"OPTIONS", 405},
-          {"PUT", 405},
-          {"HEAD", 200},
-          {"TRACE", 403},
-          {"PATCH", 405}
+          {"TC_2.1","GET", 200},
+          {"TC_2.2","POST", 405},
+          {"TC_2.3","DELETE", 405},
+          {"TC_2.4","OPTIONS", 405},
+          {"TC_2.5","PUT", 405},
+          {"TC_2.6","HEAD", 200},
+          {"TC_2.7","TRACE", 403},
+          {"TC_2.8","PATCH", 405}
         };
     return statusCode;
   }
 
   @Test(dataProvider = "dataSetStatusCodes")
-  public void testHttpMethods(String methodName, int httpCode) {
+  public void testHttpMethods(String testCaseNumber, String methodName, int httpCode) {
     Response response = given().when().request(methodName, BASE_URL);
-
-    assertEquals(response.statusCode(), httpCode, "Wrong response status code.");
+    assertEquals(response.statusCode(), httpCode, testCaseNumber+"Wrong response status code.");
   }
 }
